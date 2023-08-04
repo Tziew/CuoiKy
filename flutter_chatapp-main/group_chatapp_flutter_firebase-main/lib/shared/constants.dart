416a6e68cc1fab71b9ec153b740a7e7d092aa94c
tuilahungquoc <nguyenphuoctrieu2311@gmail.com>
2023-08-05 00:35:39 +0700

    fix complete

---
+++ flutter_chatapp-main/group_chatapp_flutter_firebase-main/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:909863140226:web:c417e43bedd67c4a8bed9b";
+  static String apiKey = "AIzaSyAkTK8j7r0kzHWwtQg8FG5ijzwLzo5skeI";
+  static String messagingSenderId = "909863140226";
+  static String projectId = "chatappfirebase-811f2";
+  final primaryColor = const Color(0xFFee7b64);
+}
